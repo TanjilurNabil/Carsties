6b05ac9ed16f759a1c0d6f470a6ae0c5d1a62ca7
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2024-06-26 11:42:13 +0600

    Consuming faulty messages

--- src/SearchService/Consumers/AuctionCreatedConsumers.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumers.cs
@@ -19,6 +19,10 @@ namespace SearchService.Consumers
             Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
 
             var item = _mapper.Map<Item>(context.Message);
+            if (item.Model=="Foo")
+            {
+                throw new ArgumentException("==>Cannot sell cars with name Foo");
+            }
 
             await item.SaveAsync();
         }
